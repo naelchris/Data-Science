af20e277894dd345ab35bad936eb22675cf168a5
nael <naelchris@gmail.com>
2021-02-01 01:18:13 +0700

    day-1

---
+++ R_fundamental.R
@@ -0,0 +1,21 @@
+#this is lec 1, code by naelchris
+A <- matrix(data = 1:25, nrow = 5, ncol = 5, byrow = TRUE)
+a <- 1
+b <- 1
+c <- -1
+#<- assign values to variables we can also use = 
+
+#predefined function
+print(log(8))
+print(log(a))
+
+#you can add short hand for help() with ? . note : when working with workspace we have to use print() to acctually see something in the console aka like console.log()
+print(?log) #we can also use help()
+print(args(log)) #args for checking argument of function
+print(log(8,base=2))
+print(log(16,base=2))
+
+#pre build objects
+data() #pre package dataset in R, built-in function
+co <- co2 #co2 is a dataset object , built-in dataset
+
